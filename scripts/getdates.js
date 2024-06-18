8d7fe11e9d724c2bcebb70ebd38405080245b91d
vickhail <esc23010@byui.edu>
2024-06-18 02:31:20 -0400

    major changes to assignment

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-    const yearSpan = document.getElementById('2024');
-    const currentYear = new Date().getFullYear();
-    yearSpan.textContent = currentYear;
-
-    const lastModifiedSpan = document.getElementById('06/15/2024');
-    lastModifiedSpan.textContent = `Last Modified: ${document.lastModified}`;
+    const modificationDate = new Date(document.lastModified);
+    const formattedDate = modificationDate.toLocaleString('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    });
+    document.getElementById("modification-date").textContent = formattedDate;
 });
